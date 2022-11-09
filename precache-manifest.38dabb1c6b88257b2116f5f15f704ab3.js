1f36794df73d272c4626e3ea24d2de1f1345f887
Timen van Gelderen <tvgelderen@outlook.com>
2022-11-09 12:57:20 +0100

    Updates

---
+++ precache-manifest.38dabb1c6b88257b2116f5f15f704ab3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4417ee67294a587e201a",
+    "url": "/crypto-watch/static/css/main.ade5018c.chunk.css"
+  },
+  {
+    "revision": "4417ee67294a587e201a",
+    "url": "/crypto-watch/static/js/main.4417ee67.chunk.js"
+  },
+  {
+    "revision": "f1e1ec423c77ce916650",
+    "url": "/crypto-watch/static/css/1.32886978.chunk.css"
+  },
+  {
+    "revision": "f1e1ec423c77ce916650",
+    "url": "/crypto-watch/static/js/1.f1e1ec42.chunk.js"
+  },
+  {
+    "revision": "1493b6731251bb1a9e1c",
+    "url": "/crypto-watch/static/js/runtime~main.1493b673.js"
+  },
+  {
+    "revision": "78fe45e630bd34b7f21b5e658fb62d69",
+    "url": "/crypto-watch/index.html"
+  }
+];
